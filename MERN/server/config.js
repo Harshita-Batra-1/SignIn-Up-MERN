9a8ef3e692bc67ad818a11ed24188f524e740c4b
Pushpit batra <pushpitbatra1@gmail.com>
2023-11-29 11:54:27 +0530

    Initial commit

---
+++ MERN/server/config.js
@@ -0,0 +1,6 @@
+export default{
+    JWT_SECRET : "qoVKLU;LzTD7BPY|ei5o$*Spd>Yrz71Z",
+    EMAIL : "ewell2@ethereal.email",   // testing email & password
+    PASSWORD : "66rSjXJY9H4JwC8Xqq",
+    ATLAS_URI : "mongodb+srv://batraharshita2:KFxiHln84QHyiGX5@cluster0.5bwhbfu.mongodb.net/?retryWrites=true&w=majority"
+}
